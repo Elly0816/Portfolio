10014c113a296e8e7ee9532ad93a88ea82924898
Eleazar Udo <elzoremmanuel@gmail.com>
2023-05-09 21:24:35 +0300

    project tiles started

--- src/app/(components)/Project/Project.tsx
+++ src/app/(components)/Project/Project.tsx
@@ -30,7 +30,7 @@ const Project: React.FC<{project: projectType}> = ({project}:{project:projectTyp
 
 
 
-return <div style={{backgroundImage: project.img}} className={`${khandFont.className} w-96 h-96 project rounded-lg flex flex-col py-3 px-3 border-solid border-black shadow-lg my-3 mx-3`}>
+return <div style={{backgroundImage: project.img}} className={`${khandFont.className} ${classes.singleProject} w-96 h-96 project rounded-lg flex flex-col py-3 px-3 border-solid border-black shadow-lg my-4 mx-3`}>
         <div className={`${shrik.className} text-xl`}>
             <h3>{project.title}</h3>
         </div>
