34f01c11f64c20d46821e434ee33b11daa723090
Eleazar Udo <elzoremmanuel@gmail.com>
2023-09-13 23:50:10 +0300

    added twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,33 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata:Metadata = {
   title: 'Eleazar Udo',
-  description: "Welcome to Eleazar Udo's portfolio website",
+  description: `Hi!
+  I'm Eleazar Udo
+  A full-stack web developer and software engineer
+  I create websites and applications that are fast, responsive and user-friendly.`,
   icons: {
     icon: '/Logo.png',
     shortcut: '/Logo.png',
     apple: '/Logo.png'
-  }
+  },
+  twitter: {card: 'summary', site: '', title: 'Eleazar Udo', description: `Hi!
+  I'm Eleazar Udo
+  A full-stack web developer and software engineer
+  I create websites and applications that are fast, responsive and user-friendly.`},
+
 }
 
+/*
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@flickr" />
+<meta name="twitter:title" content="Small Island Developing States Photo Submission" />
+<meta name="twitter:description" content="View the album on Flickr." />
+<meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
+
+
+
+*/
 
 
 
